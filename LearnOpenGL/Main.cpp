4b649de3d7153c8b890995556fad6d327d297f04
Samuel <70766878+swzwij@users.noreply.github.com>
2025-06-04 22:29:02 +0200

    Added abstract shader header

--- LearnOpenGL/Main.cpp
+++ LearnOpenGL/Main.cpp
@@ -1,18 +1,23 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout(location = 0) in vec3 aPos;\n"
+"layout(location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   ourColor = aColor;\n"
 "}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0, 0.5, 0.0, 1.0);\n"
+"   FragColor = vec4(ourColor, 1.);\n"
 "}\0";
 
 void FramebufferSizeCallBack(GLFWwindow* window, int width, int height);
@@ -45,9 +50,9 @@ int main()
 
 	float vertices[] =
 	{
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
+		-0.5f, -0.5f, 0.0f, 1., 0., 0.,
+		 0.5f, -0.5f, 0.0f, 0., 1., 0.,
+		 0.0f,  0.5f, 0.0f, 0., 0., 1.
 	};
 
 	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -79,8 +84,11 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -90,6 +98,7 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shaderProgram);
+
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
